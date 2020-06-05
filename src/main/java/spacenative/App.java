f74a7c4906867af6d2f270fe31f181b013467b25
Irene <mamaniirene@hotmail.com>
2020-06-05 23:49:04 +0200

    first commit

---
+++ src/main/java/spacenative/App.java
@@ -0,0 +1,13 @@
+package spacenative;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
